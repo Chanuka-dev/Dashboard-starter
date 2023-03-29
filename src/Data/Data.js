05c4ec0f4c5a97e9f1161b872235d3f2414d7a6d
Chanuka-dev <chanukasamarasinghe31@gmail.com>
2023-03-29 22:45:22 +0530

    Proshop

---
+++ src/Data/Data.js
@@ -0,0 +1,31 @@
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+// Side bar data...........
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Orders",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Custermes",
+  },
+  {
+    icon: UilPackage,
+    heading: "Product",
+  },
+  {
+    icon: UilChart,
+    heading: "Analitice",
+  },
+];
